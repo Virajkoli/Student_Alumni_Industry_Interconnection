0474afbcdea25f17737e0cfd94602ee5c365c7ab
durgeshanilpatil <patildurgesh0807@gmail.com>
2025-06-25 11:31:18 +0530

    Created college page and Homepage for navigating to all 3 pages

--- ElectrosoftAlumni/src/App.jsx
+++ ElectrosoftAlumni/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import StartupProfilePage from "./pages/startup/StartupProfilePage";
 import IndustryProfilePage from "./pages/industry/IndustryProfilePage";
 
+// Placeholder for College page
+const CollegePage = () => (
+  <div className="min-h-screen flex items-center justify-center text-3xl font-bold text-gray-700">
+    College Page Coming Soon
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <StartupProfilePage />
-      <IndustryProfilePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/startup" element={<StartupProfilePage />} />
+        <Route path="/industry" element={<IndustryProfilePage />} />
+        <Route path="/college" element={<CollegePage />} />
+      </Routes>
+    </Router>
   );
 }
 
